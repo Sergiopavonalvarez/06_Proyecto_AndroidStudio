790cb398574551b0bfca37d265d95ce9e386cf96
sergiopavonalvarez <aergiopavonalvarez@gmail.com>
2023-11-15 23:37:08 +0100

    23:36 15/11

--- app/src/main/java/com/example/a06_saludoconresult/MainActivity.java
+++ app/src/main/java/com/example/a06_saludoconresult/MainActivity.java
@@ -8,22 +8,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button segundaactividad;
+    TextView n;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        segundaactividad=findViewById(R.id.Segunda_Actividad);
+        n=findViewById(R.id.todavianosetunombre);
+        String nombre=getIntent().getStringExtra("Nombre");
+        if (nombre==null){
+            n.setText("Aun no se tu nombre");
+
+        }else{
+        n.setText("Bienvenido "+nombre);}
     }
-    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode()==RESULT_OK){
 
-                    }
+    public void h(View vista){
+       Intent intent=new Intent(this,MainActivity2.class);
+       startActivity(intent);
 
-                }
-            });
+    }
+
 }
--- app/src/main/java/com/example/a06_saludoconresult/MainActivity2.java
+++ app/src/main/java/com/example/a06_saludoconresult/MainActivity2.java
@@ -1,14 +1,30 @@
 package com.example.a06_saludoconresult;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    EditText a;
+    Button l;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        a=findViewById(R.id.edittext_nombre);
+        l=findViewById(R.id.button_nombre);
+    }
+
+    public void l (View vista){
+        Intent intent=new Intent(this, MainActivity.class);
+        intent.putExtra("Nombre",a.getText().toString());
+        startActivity(intent);
+
     }
 }
